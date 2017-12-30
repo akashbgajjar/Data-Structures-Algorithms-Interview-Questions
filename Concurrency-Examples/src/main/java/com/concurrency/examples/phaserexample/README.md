6d81000352ef0107ae797dac935e8d9fcea4b6cb
akashbgajjar <akashbgajjar@gmail.com>
2017-12-30 09:59:34 -0800

    Adding content to ReadMe

--- Concurrency-Examples/src/main/java/com/concurrency/examples/phaserexample/README.md
+++ Concurrency-Examples/src/main/java/com/concurrency/examples/phaserexample/README.md
@@ -1,1 +1,16 @@
-Akash Gajjar
+Phaser:
+
+Java Doc: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/Phaser.html
+
+Description:
+	In this example main thread is creating 3 child threads and initialize phaser that will wait for all child threads.
+	
+	Main Thread
+	|
+	Thread_1
+	|
+	Thread_2
+	|
+	Thread_3
+		\
+		 Thread_4
---
+++ Concurrency-Examples/src/main/java/com/concurrency/examples/phaserexample/Thread_4.java
@@ -0,0 +1,25 @@
+package com.concurrency.examples.phaserexample;
+
+import java.util.concurrent.Phaser;
+
+public class Thread_4 extends AbstractRunnable{
+
+	private final long timeout = 1000L;
+	
+	public Thread_4(Phaser phaser) {
+		super(phaser);
+	}
+
+	@Override
+	public void execute() {
+		try{
+			System.out.println("Thread_4 waiting for " + timeout + " ms");
+			Thread.sleep(timeout);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			System.out.println("Thread_4 party arrived");
+		}
+	}
+
+}
