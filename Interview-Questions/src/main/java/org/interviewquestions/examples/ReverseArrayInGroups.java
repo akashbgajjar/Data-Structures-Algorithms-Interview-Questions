9f678a5375a8b8eb5e62bc42343d8f39411a9807
akashbgajjar <akashbgajjar@gmail.com>
2018-03-17 10:41:18 -0700

    More Examples

--- Interview-Questions/src/main/java/org/interviewquestions/examples/ReverseArrayInGroups.java
+++ Interview-Questions/src/main/java/org/interviewquestions/examples/ReverseArrayInGroups.java
@@ -5,94 +5,97 @@ package org.interviewquestions.examples;
  * @author Akash Gajjar
  * 
  * 
- * Given an array, reverse every sub-array formed by consecutive k elements.
-
-	Input:
-	
-	The first line of input contains a single integer T denoting the number of test cases. Then T test cases follow. Each test case consist of two lines. The first line of each test case consists of an integer N, where N is the size of array.The second line of each test case contains N space separated integers denoting array elements.The third line of each test case consists of an integer K.
-	
-	Output:
-	Corresponding to each test case, in a new line, print the modified array.
-	
-	Constraints:
-	
-	1 <= T <= 100
-	1 <= N <= 500
-	1 <= A[i] <= 500
-	
-	Example:
-	
-	Input
-	1
-	5
-	1 2 3 4 5
-	3
-	
-	Output
-	3 2 1 5 4
- *
+ *         Given an array, reverse every sub-array formed by consecutive k
+ *         elements.
+ * 
+ *         Input:
+ * 
+ *         The first line of input contains a single integer T denoting the
+ *         number of test cases. Then T test cases follow. Each test case
+ *         consist of two lines. The first line of each test case consists of an
+ *         integer N, where N is the size of array.The second line of each test
+ *         case contains N space separated integers denoting array elements.The
+ *         third line of each test case consists of an integer K.
+ * 
+ *         Output: Corresponding to each test case, in a new line, print the
+ *         modified array.
+ * 
+ *         Constraints:
+ * 
+ *         1 <= T <= 100 1 <= N <= 500 1 <= A[i] <= 500
+ * 
+ *         Example:
+ * 
+ *         Input 1 5 1 2 3 4 5 3
+ * 
+ *         Output 3 2 1 5 4
+ * 
  */
 
 public class ReverseArrayInGroups {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int a[] = new int[500];
-		for(int i=1; i<=500; i++){
-			a[i-1] = i; 
+		for (int i = 1; i <= 500; i++) {
+			a[i - 1] = i;
 		}
 		int k = 320;
 		long start = System.currentTimeMillis();
 		int b[] = getReverseArrayInGroups(a, k);
 		System.out.println(System.currentTimeMillis() - start);
-		
-		for(int i=0; i<b.length; i++){
+
+		for (int i = 0; i < b.length; i++) {
 			System.out.print(b[i]);
-			if(i < b.length - 1){
+			if (i < b.length - 1) {
 				System.out.print(" ");
 			}
 		}
-		
+
 	}
-	
-	private static int[] getReverseArrayInGroups(int arr[], int k){
-		
-		if(arr != null && arr.length > 0){
-			
-			Integer position = getPositionOfKthElement(arr, 0, arr.length - 1, k);
-			
-			if(position != null){
-				for(int i=0,j=position; i<position+1 && j>0 && i<j; i++,j--){
+
+	private static int[] getReverseArrayInGroups(int arr[], int k) {
+
+		if (arr != null && arr.length > 0) {
+
+			Integer position = getPositionOfKthElement(arr, 0, arr.length - 1,
+					k);
+
+			if (position != null) {
+				for (int i = 0, j = position; i < position + 1 && j > 0
+						&& i < j; i++, j--) {
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
 				}
-				for(int i=position+1,j=arr.length-1; i<arr.length && j>position && i<j; i++,j--){
+				for (int i = position + 1, j = arr.length - 1; i < arr.length
+						&& j > position && i < j; i++, j--) {
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
 				}
 			}
-			
+
 		}
-		
+
 		return arr;
 	}
-	
-	private static Integer getPositionOfKthElement(int arr[], int s_index, int e_index, int k){
-		int find = ((e_index + s_index) + 1)/2;
-		
-		if(find < arr.length && find >= 0){
-			if(arr[find] == k){
+
+	private static Integer getPositionOfKthElement(int arr[], int s_index,
+			int e_index, int k) {
+		int find = ((e_index + s_index) + 1) / 2;
+
+		if (find < arr.length && find >= 0) {
+			if (arr[find] == k) {
 				return find;
-			}else if(arr[find] < k){
+			} else if (arr[find] < k) {
 				s_index = find + 1;
 				return getPositionOfKthElement(arr, s_index, e_index, k);
-			}else if(arr[find] > k){
+			} else if (arr[find] > k) {
 				e_index = find - 1;
 				return getPositionOfKthElement(arr, s_index, e_index, k);
 			}
 		}
-		
+
 		return null;
 	}
 }
---
+++ Interview-Questions/src/main/java/org/interviewquestions/examples/TrappingRainWater.java
@@ -0,0 +1,61 @@
+package org.interviewquestions.examples;
+
+/**
+ * 
+ * @author Akash Gajjar
+ * 
+ *         Given n non-negative integers in array representing an elevation map
+ *         where the width of each bar is 1, compute how much water it is able
+ *         to trap after raining. For example: Input: 3 2 0 2 Output: 2
+ *         Structure is like below | | |_| We can trap 2 units of water in the
+ *         middle gap.
+ * 
+ *         Below is another example.
+ * 
+ *         Input: The first line of input contains an integer T denoting the
+ *         number of test cases. The description of T test cases follows. Each
+ *         test case contains an integer N followed by N numbers to be stored in
+ *         array.
+ * 
+ *         Output: Print trap units of water in the middle gap.
+ * 
+ *         Constraints: 1<=T<=100 3<=N<=100 0<=Arr[i]<10
+ * 
+ *         Example: Input: 2 4 7 4 0 9 3 6 9 9
+ * 
+ *         Output: 10 0
+ * 
+ *         https://practice.geeksforgeeks.org/problems/trapping-rain-water/0
+ * 
+ */
+public class TrappingRainWater {
+
+	public static void main(String[] args) {
+		int a[] = { 10, 1, 6, 5, 2, 0, 8, 3, 0, 1 };
+		System.out.println(getUnitsOfWater(a, a.length));
+	}
+
+	private static int getUnitsOfWater(int arr[], int n) {
+		int left[] = new int[n];
+
+		int right[] = new int[n];
+
+		int water = 0;
+
+		left[0] = arr[0];
+		for (int i = 1; i < n; i++) {
+			left[i] = Math.max(left[i - 1], arr[i]);
+		}
+
+		right[n - 1] = arr[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			right[i] = Math.max(right[i + 1], arr[i]);
+		}
+
+		for (int i = 0; i < n; i++) {
+			water += Math.min(left[i], right[i]) - arr[i];
+		}
+
+		return water;
+	}
+}
