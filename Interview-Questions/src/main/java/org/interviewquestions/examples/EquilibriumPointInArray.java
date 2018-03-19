bb6f7e8b868edd6cca3284816954bde65ca6ee1c
akashbgajjar <akashbgajjar@gmail.com>
2018-03-19 15:02:51 -0700

    Changes

--- Interview-Questions/src/main/java/org/interviewquestions/examples/EquilibriumPointInArray.java
+++ Interview-Questions/src/main/java/org/interviewquestions/examples/EquilibriumPointInArray.java
@@ -3,7 +3,8 @@ package org.interviewquestions.examples;
 /**
  * 
  * @author Akash Gajjar
- * Given an array A your task is to tell at which position the equilibrium first occurs in the array. Equilibrium position in an array is a position such that the sum of elements below it is equal to the sum of elements after it.
+ * Given an array A your task is to tell at which position the equilibrium first occurs in the array. 
+ * Equilibrium position in an array is a position such that the sum of elements below it is equal to the sum of elements after it.
 
  * Input:
  * The first line of input contains an integer T denoting the no of test cases then T test cases follow. First line of each test case contains an integer N denoting the size of the array. Then in the next line are N space separated values of the array A.
@@ -20,7 +21,7 @@ package org.interviewquestions.examples;
 public class EquilibriumPointInArray {
 
 	public static void main(String[] args){
-		int a[] = {7,1,8};
+		int a[] = {-7, 1, 5, 2, -4, 3, 0};
 		System.out.println(getEquilibriumPoint(a));
 	}
 	
@@ -31,33 +32,26 @@ public class EquilibriumPointInArray {
 				return arr[0];
 			}
 			
-			int l_sum = 0;
-			int r_sum = 0;
-			for(int i=0,j=(arr.length - 1);i<j;){
-				if(i == 0 && j == arr.length - 1){
-					l_sum = arr[i];
-					r_sum = arr[j];
+			int sum = 0;
+			int left_sum = 0;
+			
+			for(int i=0; i<arr.length; i++){
+				sum += arr[i];
+			}
+			
+			for(int i=0; i<arr.length; i++){
+				
+				sum -= arr[i];
+				
+				if(left_sum == sum){
+					return arr[i];
 				}
-				if(l_sum > r_sum){
-					r_sum += arr[--j];
-				}else if(l_sum < r_sum){
-					l_sum += arr[++i];
-				}else{
-					if(i < j){
-						if(j-1 == i+1)
-							return arr[i+1];
-						else{
-							r_sum += arr[--j];
-							l_sum += arr[++i];
-						}
-					}else{
-						return null;
-					}
-				}
+				
+				left_sum += arr[i];
 			}
 		}
 		
-		return null;
+		return -1;
 	}
 	
 }
